9ae6aaf2c6018b3693747ccaea4fc596dfe85354
Sandip Bera <sandipbera35@gmail.com>
2024-03-03 22:34:14 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Println("Demo Project")
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
+
+}
