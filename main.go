7f27c0d0c5e7d8e672f911408100a89106206f0c
Sandip Bera <sandipbera35@gmail.com>
2024-03-10 21:20:22 +0530

    jsonreturn tested

--- main.go
+++ main.go
@@ -6,13 +6,22 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Data struct {
+	Name  string
+	Email string
+}
+
 func main() {
 	fmt.Println("Demo Project")
 
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		var v1 Data
+		v1.Name = "Sandip"
+		v1.Email = "s@b.com"
+		return c.JSON(v1)
+		// return c.SendString("Hello, World!")
 	})
 
 	app.Listen(":3000")
